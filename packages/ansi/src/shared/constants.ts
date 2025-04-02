cccc6cc5f6789af0c6793ed1c64edd812b4eb436
subh <harvirbuttar2917@gmail.com>
2025-04-01 18:14:31 -0600

    Introduced new constants that may be required for more effective suffix

--- packages/ansi/src/shared/constants.ts
+++ packages/ansi/src/shared/constants.ts
@@ -1,11 +1,16 @@
 const ANSI_COLORS = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'] as const;
+
 const ANSI_STYLES = ['reset', 'bold', 'dim', 'italic', 'underline', 'blink', 'flashBlink', 'reverse', 'hidden', 'strikethrough'] as const;
 const DEFAULT_ANSI_PARAMETERS = {
     ST: 0, FG: 39, BG: 49
 };
 
-/**Offsets */
+const RESET_ST_CODES = [0, 22, 22, 23, 24, 25, 25, 27, 28, 29] as const;
+const RESET_FG_CODE = 39;
+const RESET_BG_CODE = 49;
 
+
+/**Offsets */
 const CL_OFFSET = 30;
 const BR_CL_OFFSET = 90;
 const BG_OFFSET = 40;
@@ -16,6 +21,10 @@ export {
     ANSI_COLORS,
     ANSI_STYLES,
     DEFAULT_ANSI_PARAMETERS,
+
+    RESET_ST_CODES,
+    RESET_FG_CODE,
+    RESET_BG_CODE,
 
     CL_OFFSET,
     BR_CL_OFFSET,
